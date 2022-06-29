7b9ed27bbeb2fd07505e1669ac848bfd8c3eb6c7
Abdulbosit-AiM <abdulbosit.melikuziev@gmail.com>
2022-06-29 11:56:54 +0500

    finish Board class + new component

--- src/components/BoardComponent.tsx
+++ src/components/BoardComponent.tsx
@@ -1,9 +1,27 @@
 import React  from "react";
+import { Board } from "../models/Board";
+import type { Cell } from "../models/Cell";
+import CellComponent from "./CellComponent";
+
+interface BoardProps {
+    board: Board
+    setBoard: (board: Board) => void
+}
 
 const BoardComponent = () => {
     return (
         <div className="board">
+            {Board.cells.map((row: Cell, index: number) => 
+                <React.Fragment key={index}>
+
+                    {row.map(cell => 
+                        <CellComponent 
+
+                        />
+                    )}
             
+                </React.Fragment>
+            )}
         </div>
     )
 }
--- src/models/Board.ts
+++ src/models/Board.ts
@@ -1,4 +1,5 @@
 import {Cell} from "./Cell"
+import {Colors} from "./Colors"
 
 export class Board {
     cells: Cell[][] = []
@@ -8,9 +9,9 @@ export class Board {
         const row: Cell[] = []
         for (let j = 0; j < 8; j++){
             if ((i + j) % 2 !== 0) {
-                row.push(new Cell()) // Black Cells
+                row.push(new Cell(this, j, i, Colors.BLACK, figure: null)) // Black Cells
             } else {
-                row.push(new Cell()) // White Cells
+                row.push(new Cell(this, j, i, Colors.WHITE, figure: null)) // White Cells
             }
         }
         this.cells.push(row)
